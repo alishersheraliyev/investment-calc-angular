fb4991465bfcc0d2d2d7b752d48b26d15d0030e5
alishersheraliyev <81514415+alishersheraliyev@users.noreply.github.com>
2026-06-10 22:33:35 +0500

    some

---
+++ src/app/investment-input.model.ts
@@ -0,0 +1,6 @@
+export interface InvestmentInputType {
+  initialInvestment: number;
+  annualInvestment: number;
+  expectedReturn: number;
+  duration: number;
+}
--- src/app/user-input/user-input.component.ts
+++ src/app/user-input/user-input.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import type { InvestmentInputType } from '../investment-input.model';
 
 @Component({
   selector: 'app-user-input',
@@ -9,12 +10,18 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './user-input.component.css',
 })
 export class UserInputComponent {
+  @Output() calculate = new EventEmitter<InvestmentInputType>();
   enteredInitialInvestment = '0';
   enteredAnnualInvestment = '0';
   enteredExpectedReturn = '5';
   enteredDuration = '10';
 
   onSubmit() {
-    console.log('Form submitted!');
+    this.calculate.emit({
+      initialInvestment: parseFloat(this.enteredInitialInvestment),
+      annualInvestment: parseFloat(this.enteredAnnualInvestment),
+      expectedReturn: parseFloat(this.enteredExpectedReturn),
+      duration: parseInt(this.enteredDuration),
+    });
   }
 }
